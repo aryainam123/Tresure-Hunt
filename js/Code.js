bc277c1ac92fb001c8ee1ac578f31434b61880f6
Arya Inamdar <aryainamdar2009@gmail.com>
2021-06-17 09:04:27 +0530

    tresure hunt

---
+++ js/Code.js
@@ -0,0 +1,20 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    fill("white")
+    textSize(15)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    
+fill("white")
+    textSize(15)
+    fill("lightblue")
+    text("Hint: Perfoms a particular task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,330)
+}
